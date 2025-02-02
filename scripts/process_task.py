9eced8363dd4696b2a884fe1c5b56fa6f5b6ef79
David Marx <david.marx84@gmail.com>
2025-02-02 11:26:47 -0800

    Update process_task.py

--- scripts/process_task.py
+++ scripts/process_task.py
@@ -4,15 +4,28 @@ import json
 
 from duckduckgo_search import DDGS
 import fire
+#from gh_store.core.access import AccessControl
 from loguru import logger
-# TODO: import interfaces for compatibility
+# TODO: import abstract interfaces for compatibility
+
+#access_control = AccessControl(self.repo)
+
+# TODO: access validation as gh-store CLI capability
+# issue = self.repo.get_issue(issue_number)
+#         if not self.access_control.validate_issue_creator(issue):
 
 ddg = DDGS()
 
+def image_search(**kwargs):
+    results = ddg.images
+    outstr="  \n".join([f"[{r.title}](r.image)" for r in results])
+    return outstr
+
 OPERATORS={
-    "ddg.images": ddg.images,
+    "ddg.images": image_search,
     "ddg.chat": ddg.chat,
 }
+
 
 @dataclass
 class TaskConfig:
@@ -21,7 +34,6 @@ class TaskConfig:
 
 def main(config: dict):
     config = TaskConfig(**config)
-    logger.info("hello gh-tasks")
     logger.info(config)
     op = OPERATORS[config.operator]
     result = op(**config.kwargs)
