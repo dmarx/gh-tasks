31416e2422af2ee65f8daeb6916be108ffe69fbb
David Marx <david.marx84@gmail.com>
2025-02-02 10:56:50 -0800

    Update process_task.py

--- scripts/process_task.py
+++ scripts/process_task.py
@@ -1,9 +1,31 @@
 # scripts/process_task.py
+from dataclasses import dataclass
+import json
+
+from duckduckgo_search import DDG
 import fire
+import loguru
 # TODO: import interfaces for compatibility
 
-def main(config):
-    print("hello gh-tasks")
-    print(config)
+ddg = DDG()
+
+OPERATORS={
+    "ddg.image": ddg.image,
+    "ddg.chat": ddg.chat,
+}
+
+@dataclass
+class TaskConfig:
+    operator: str
+    kwargs: dict
+
+def main(config: Config):
+    config = Config(**json.loads(config))
+    loguru.info("hello gh-tasks")
+    loguru.info(config)
+    op = OPERATORS[config.operator]
+    result = op(**kwargs)
+    logger.info(result)
+    return result
 
 fire.Fire(main)
